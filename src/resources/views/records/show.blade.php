b4addbfc110974eae4cd326043d8d2f101713ddd
Eric Scuccimarra <skooch@gmail.com>
2017-02-13 11:17:04 +0100

    Fixed bad URI

--- src/resources/views/records/show.blade.php
+++ src/resources/views/records/show.blade.php
@@ -36,7 +36,7 @@
 							@if($record->discogs)
 								<div class="row">
 									<div class="col-md-3"><strong>{{ trans('record-lang::records.link') }}:</strong></div>
-									<div class="col-md-9"><a href="http://www.discogs.com/{{ $record->discogs }}">{{ $record->discogs }}</a></div>
+									<div class="col-md-9"><a href="http://www.discogs.com{{ $record->discogs }}">{{ $record->discogs }}</a></div>
 								</div>
 							@endif
 							{{-- <div class="row">
